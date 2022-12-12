cc7c3ad75d28e587d5f6ad8afa0893f40fc0e591
muradorucov <orucowmurad@gmail.com>
2022-12-12 21:36:39 +0400

    feat : js lesson-01 added

---
+++ JavaScript/Lesson-01/app.js
@@ -0,0 +1,75 @@
+//Adlandırma məqalə
+
+
+
+
+
+// console.log("App.js Hello Dunya");
+// console.error("Bu bir Errordur");
+// console.warn("Bu bir xəbərdarlıqdır!");
+
+
+// let
+// var
+// const
+
+
+// let firstName = "Murad";
+// console.log('1-ci firstName', firstName);
+
+// let firstName2 = firstName;
+// console.log("2-ci firsTName",firstName2);
+// console.log(firstName);
+
+// firstName = "Taleh";
+
+// var lastName = "Orucov";
+// console.log(lastName);
+
+// const company = "IT Academy";
+// console.log(company);
+
+// company = "Alqoritmika"
+// console.log(company);
+
+// console.log("1-ci deyishdi", firstName);
+// console.log("2-ci firsTName",firstName2);
+
+
+// firstName = "Amil";
+
+
+// console.log(b);
+// var b = 44
+
+// console.log(a);
+// let a = 45
+
+// let qwegh1 = "dfref"
+
+// console.log(qwegh1)
+
+// let name  = "Murad"
+// const name3  ="Murad"
+// name  ="Murad"
+
+// console.log(name);
+
+
+// var firstNameAndLastName
+
+
+
+
+// let number1 = "Murad";
+// let number2 = "Orucov";
+
+// console.log(number1+ " " + number2);
+
+// number1 = -56;
+// number2 = 45;
+// emil = number1 + number2
+// console.log(c + number1);
+// console.log(number1 - number2);
+// console.log(number1 / number2);
+// console.log(number1 * number2);
