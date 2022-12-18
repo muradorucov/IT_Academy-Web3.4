71507d5d3ca27fd564385c40fb56c91e6fb4e4c2
muradorucov <orucowmurad@gmail.com>
2022-12-18 12:33:32 +0400

    commit

---
+++ JavaScript/Lesson-05/app.js
@@ -0,0 +1,132 @@
+// let a = 5;
+// let b = '40'
+
+// console.log("a böyükdürmü b?", a > b);//false
+// console.log("a kiçikdirmi b?",a < b);//true
+// console.log("a böyük bərabərdirmi b?",a >= b);//false
+// console.log("a kiçik bərabərdirmi b?",a <= b);//ture
+
+// typeof()
+
+// let c = 0;
+// let d = null;
+
+// console.log(c == d)//true
+// console.log(c === d)//false
+
+// console.log(c != d)
+// console.log(c !== d)
+
+// let value = 10
+// let value = 20
+
+// console.log(value < 15);
+// console.log(value <= 19 )
+
+// let a = 5;
+// let b = 6;
+
+
+// console.log((a > 4 && b < 5) || a >= b);
+
+// let num = prompt()
+
+// if (num < 10) {
+//     alert("num 10-dan kicikdir")
+
+// } else if (num == 10) {
+//     alert("num 10a beraberdir")
+// } else if (num == 12) {
+//     alert("num 12-e beraberdir")
+// }
+// else (num==13) {
+//     alert("num 10-dan boyukdur")
+// }
+
+// let num = 12;
+
+// if (num) {
+//     // console.log(num);
+//     let num = 5
+//     // console.log(num);
+
+// }
+
+
+// console.log(num)
+
+// num = 4;
+// switch (num) {
+//     case 4:
+//         console.log(num);
+//         break;
+//     default:
+//         console.log("default", num);
+//         // Əgər şərtlərdən heç biri yerinə yetirilməyibsə
+//         break;
+// }
+
+// let weekDay = +prompt();
+
+// switch (weekDay) {
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//         alert('Iş günü')
+//         break;
+//     case 6:
+//     case 7:
+//         alert('Həftə sonu')
+//         break;
+//     default:
+//         alert("Yanlış dəyər")
+
+// }
+
+// if (weekDay == '1') {
+//     alert("Bazarertəsi")
+// } else if (weekDay == '2') {
+//     alert('çərşənbə axşamı')
+// } else if (weekDay == '3') {
+//     alert('çərşənbə')
+// } else if (weekDay == '4') {
+//     alert('cümə axşamı')
+// } else if (weekDay == '5') {
+//     alert('cümə')
+// } else if (weekDay == '6') {
+//     alert('Şənbə')
+// } else if (weekDay == '7') {
+//     alert('Bazar')
+// } else {
+//     alert("Yanlış dəyər")
+// }
+
+
+let isStock = true;
+
+// let message =
+
+//     console.log(message);
+
+// console.log(`<p>${isStock ? "Stockda var" : "stockda yoxdur"}</p>`);
+
+
+let gender = prompt()
+// let a = "a"
+// console.log(a);
+// let A = "A"
+// console.log(A);
+
+
+// switch(gender){
+//     case 'M':
+//         alert("Male")
+//         break;
+//     case 'F':
+//         alert("Female")
+//         break;
+//     default:
+//         alert("yanlish deyer")
+// }
