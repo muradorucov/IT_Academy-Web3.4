a2a1e5ca753080d64a5713a79f68692c9cb63637
muradorucov <orucowmurad@gmail.com>
2023-01-09 23:08:10 +0400

    feat: Lesson-11 added

---
+++ JavaScript/Lesson-11/main.js
@@ -0,0 +1,41 @@
+// let sum = 1;
+
+
+// for (let i = 1; i <= 10; i++) {
+//     sum *= i
+//     console.log(`index = ${i} olanda hasil = ${sum}`)
+
+// }
+
+// console.log(sum)
+
+// let str = prompt();
+
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str[i]);
+// }
+
+
+// let str = prompt();
+// //salam
+// for (let index = str.length - 1; index >= 0; index--) {
+//     console.log(str[index])
+
+// }
+// str = "Hello . World . Salam"
+// for (let index = 0; index < str.length; index++) {
+
+//     if (str[index] == ".") {
+//         console.log(index);
+//         continue;
+//     }
+
+//     console.log(str[index])
+
+// }
+
+
+let str = "Salam 4465 66kjkd"
+
+
+console.log(parseFloat(str))
